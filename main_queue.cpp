96e76ecc6ddf040e13e2e789798d925056e80706
AntonSmagin <back.antares@yandex.ru>
2015-10-01 17:31:32 +0300

    Update main_queue.cpp

--- main_queue.cpp
+++ main_queue.cpp
@@ -6,16 +6,8 @@ using namespace std;
 
 int main()
 {
-	Node *pbeg = First(10);
-	Node *pend = pbeg;
 
-	for (int i = 2; i < 6; i++)
-	{
-		add(&pend, i);
-
-	}
-	print(&pbeg);
-
+        menu();
 
 
 	system("pause");

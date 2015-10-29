f03c937d910abf64c2ecdfc6f304f7632349881e
AntonSmagin <back.antares@yandex.ru>
2015-10-29 16:34:17 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,8 +1,30 @@
 #include "stdafx.h"
-#include "queue.h"
+#include "stack.h"
 #include <iostream>
 
 
+
+
+/*
+	BRIEF: функция втсавки первого элемента в стек
+	IN: Число,которое станет первым элементом
+	OUT: Адресс первого элемента структуры
+*/
+node* first(int d)
+{
+	node *top=new node;
+	top->d=d;
+	top->p=0;
+	return top;
+}
+
+
+
+/*
+	BRIEF: Функция вставки элемента в стек
+	IN: Адрес вершины,число
+	OUT: ---
+*/
 void push(node **top, int d)
 {
 	node *pv = new node;
@@ -10,20 +32,54 @@ void push(node **top, int d)
 	pv->p = *top;
 	*top = pv;
 }
-int pop(node **top)
+
+
+
+/*
+	BRIEF: Функция удаления элемента из стека
+	IN: Адрес вершины
+	OUT: ---
+*/
+void pop(node **top)
 {
-	int temp = (*top)->d;
 	node *pv = *top;
 	*top = (*top)->p;
 	delete pv;
-	return temp;
 }
-void print(node **top)
+
+
+
+/*
+	BRIEF: Функция печати стека
+	IN: Адрес вершины стека
+	OUT: Элементы стека на экран
+*/
+void print(node *top)
 {
 	node *pv = top;
-	while (pv != NULL)
+	while (pv != NULL) // Если адрес вершины не указывает на 0(т.е. есть еще элементы)
 	{
 		cout << pv->d << endl;
+		cout << " ";
 		pv = pv->p;
+	}
+}
+
+
+
+/*
+	BRIEF: Функция проверки стека на пустоту
+	IN: Адрес вершины стека
+	OUT: 0,если стек пуст,в ином случае 1
+*/
+bool isEmpty(node *top)
+{
+	if (top == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
 	}
 }

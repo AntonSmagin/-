6383bda5d9b8401d0cd5d43026db356b58e6547f
AntonSmagin <back.antares@yandex.ru>
2015-10-29 16:35:03 +0300

    Update main_stack.cpp

--- main_stack.cpp
+++ main_stack.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include "queue.h"
+#include "stack.h"
 #include <iostream>
 #include <conio.h>
 
@@ -11,35 +11,92 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int a;
-	bool fl = 1;
-	node *pbeg = NULL;
-	node *pend = NULL;
+	int a, mn;
+	node *top = NULL;
 	setlocale(LC_ALL, "Russian");
 
-	cout << "Введите число в стек,для прекращения ввода введите -1" << endl;
-	cin >> a;
-	pbeg = pend = first(a);
-	do
-	{
-		cin >> a;
-		if (a != -1)
-			add(&pend, a);
-	} while (a != -1);
-
-
 
 	do
 	{
-		cout << "Для удаление элемента нажмите 0,иначе введите 1" << endl;
-		cin >> fl;
-		if (fl == 0)
-			del(&pbeg);
-	} while (fl == 0);
-	cout << endl;
-	cout << endl;
-	print(pbeg);
+		cout << "Выберите пунк из меню:" << endl;
+		cout << "0)Выход" << endl;
+		cout << "1)Добавить элемент в стек" << endl;
+		cout << "2)Удалить элемент из стека" << endl;
+		cout << "3)Печать стека" << endl;
+		cin >> mn;
+		switch (mn)
 
+		{
+
+		case 1:
+
+		{
+			cout << "Введите число в стек:" << endl;
+			cin >> a;
+			if (isEmpty(top) == 0)
+			{
+				top = first(a);
+				system("cls");
+			}
+			else
+			{
+				push(&top, a);
+				system("cls");
+			}
+			break;
+
+		}
+
+		case 2:
+
+		{
+			if (isEmpty(top) == 0)
+			{
+				system("cls");
+				cout << "Элементов в стеке нет" << endl;
+				cout << endl;
+				cout << endl;
+				cout << endl;
+			}
+			else
+			{
+				pop(&top);
+				system("cls");
+				cout << endl;
+				cout << endl;
+				cout << endl;
+			}
+			break;
+
+		}
+
+		case 3:
+
+		{
+			if (isEmpty(top) == 0)
+			{
+				system("cls");
+				cout << "Элементов в стеке нет" << endl;
+				cout << endl;
+				cout << endl;
+				cout << endl;
+			}
+			else
+			{
+				system("cls");
+				print(&top);
+				cout << endl;
+				cout << endl;
+				cout << endl;
+			}
+			break;
+
+		}
+
+		}
+		if (mn<0 || mn>3)
+			system("cls");
+	} while (mn != 0);
 	_getch();
 	return 0;
 }
